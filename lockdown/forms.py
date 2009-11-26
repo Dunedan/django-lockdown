c312924cab5638586fea75fbc8948ca3464d9109
Chris Beaven <smileychris@gmail.com>
2009-11-26 13:34:02 +1300

    Don't render the password when the form refreshes

--- lockdown/forms.py
+++ lockdown/forms.py
@@ -2,10 +2,11 @@ from django import forms
 
 from lockdown import settings
 
+
 class LockdownForm(forms.Form):
-    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(widget=forms.PasswordInput(render_value=False))
 
     def clean_password(self):
-        if self.cleaned_data['password'] == settings.LOCKDOWN_PASSWORD:
+        if self.cleaned_data['password'] in settings.LOCKDOWN_PASSWORD:
             return self.cleaned_data['password']
         raise forms.ValidationError('Incorrect password.')
