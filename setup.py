59f791d28870b3ef76af84df69caf84832519779
Carl Meyer <carl@dirtcircle.com>
2010-01-28 16:19:09 -0500

    switch from .txt to .rst
    --HG--
    rename : AUTHORS.txt => AUTHORS.rst
    rename : CHANGES.txt => CHANGES.rst
    rename : README.txt => README.rst
    rename : TODO.txt => TODO.rst

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ try:
 except IOError:
     hgrev = ''
 
-long_description = '\n'.join([open('README.txt').read(),
-                              open('CHANGES.txt').read(),
-                              open('TODO.txt').read()])
+long_description = '\n'.join([open('README.rst').read(),
+                              open('CHANGES.rst').read(),
+                              open('TODO.rst').read()])
 setup(
     name='django-lockdown',
     version='0.1.1.post%s' % hgrev,
