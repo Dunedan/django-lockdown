403d450cdde10d67c6b5a5a82f0c03989bb27acc
Markus Kaiserswerth <github@sensun.org>
2014-03-15 17:25:27 +0100

    Added Python 2, 3 PyPI classifiers

--- setup.py
+++ setup.py
@@ -42,6 +42,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Framework :: Django',
     ],
     zip_safe=False,
