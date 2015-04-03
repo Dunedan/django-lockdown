9674fd685352c42e8f535e995771c7cda19a41a5
Daniel Roschka <danielroschka@phoenitydawn.de>
2015-04-04 01:20:49 +0200

    Remove everything related to mercurial
    django-lockdown will be developed with git only from now on, therefore old
    mercurial related stuff isn't needed anymore.

--- setup.py
+++ setup.py
@@ -2,31 +2,12 @@ from setuptools import setup, find_packages
 import subprocess
 import os.path
 
-try:
-    # don't get confused if our sdist is unzipped in a subdir of some
-    # other hg repo
-    if os.path.isdir('.hg'):
-        p = subprocess.Popen(['hg', 'parents', r'--template={rev}\n'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if not p.returncode:
-            fh = open('HGREV', 'w')
-            fh.write(p.communicate()[0].splitlines()[0])
-            fh.close()
-except (OSError, IndexError):
-    pass
-
-try:
-    hgrev = open('HGREV').read()
-    hgrev = '.post' + hgrev
-except IOError:
-    hgrev = ''
-
 long_description = '\n'.join([open('README.rst').read(),
                               open('CHANGES.rst').read(),
                               open('TODO.rst').read()])
 setup(
     name='django-lockdown',
-    version='0.1.3%s' % hgrev,
+    version='0.1.3',
     description=('Site-wide or per-view lockdown with customizable preview '
                  'authorization'),
     long_description=long_description,
