a3a11a8d723a71e00536a9acd8fd50274e648123
Daniel Roschka <danielroschka@phoenitydawn.de>
2015-04-06 20:39:43 +0200

    Release 1.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,14 +1,27 @@
 CHANGES
 =======
 
-0.1.3 (2014.03.15)
+1.1 (2015.04.06)
+------------------
+
+- Proper new version after 0.1.2 and 0.1.3 have been tagged after the release
+  of 1.0. Contains all new features of 0.1.2 and 0.1.3, most notably support
+  for Python 3.
+
+- Last version of django-lockdown with support for Django 1.3, 1.5 and 1.6.
+  Upcoming versions will only support Django versions with official security
+  support. For the time being these are Django 1.4 LTS, 1.7 and 1.8 LTS.
+
+- Fixes testing for Django >=1.7
+
+0.1.3 (2014.03.15) (never released)
 ------------------
 
 - Added ``LOCKDOWN_ENABLED`` setting.
 
 - Removed Django 1.1 backport of ``decorator_from_middleware_with_args``.
 
-0.1.2 (2014.03.15)
+0.1.2 (2014.03.15) (never released)
 ------------------
 
 - Require at least Django 1.3.
@@ -19,8 +32,11 @@ CHANGES
 
 - Minor syntax adjustments for Python 3 compatibility.
 
-- Allow multiple passwords (the passwords setting has changed from
-  ``LOCKDOWN_PASSWORD`` to ``LOCKDOWN_PASSWORDS``.
+1.0 (2013.07.10)
+------------------
+
+- BACKWARDS INCOMPATIBLE: Allow multiple passwords (the passwords setting has
+  changed from ``LOCKDOWN_PASSWORD`` to ``LOCKDOWN_PASSWORDS``).
 
 - Decorator changed to a callable decorator (so settings can be overridden for
   an individual decorator).
--- setup.py
+++ setup.py
@@ -3,29 +3,34 @@ import subprocess
 import os.path
 
 long_description = '\n'.join([open('README.rst').read(),
-                              open('CHANGES.rst').read(),
-                              open('TODO.rst').read()])
+                              open('CHANGES.rst').read()])
 setup(
     name='django-lockdown',
-    version='0.1.3',
-    description=('Site-wide or per-view lockdown with customizable preview '
-                 'authorization'),
+    version='1.1',
+    description=('Lock down a Django site or individual views, with '
+                 'configurable preview authorization'),
     long_description=long_description,
     author='Carl Meyer',
     author_email='carl@dirtcircle.com',
-    url='http://bitbucket.org/carljm/django-lockdown/',
+    url='https://github.com/Dunedan/django-lockdown/',
     packages=find_packages(),
     install_requires=['Django>=1.3'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Framework :: Django',
+        'Framework :: Django :: 1.4',
+        'Framework :: Django :: 1.5',
+        'Framework :: Django :: 1.6',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
     ],
     zip_safe=False,
     test_suite='runtests.runtests',
