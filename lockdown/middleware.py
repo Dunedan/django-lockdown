73e7f6d34e168e01d8653acd6916c87a33489c0e
Daniel Roschka <danielroschka@phoenitydawn.de>
2017-11-12 16:14:26 +0100

    Rename variable to avoid shadowing builtin

--- lockdown/middleware.py
+++ lockdown/middleware.py
@@ -22,20 +22,20 @@ def get_lockdown_form(form_path):
     if not form_path:
         raise ImproperlyConfigured('No LOCKDOWN_FORM specified.')
     form_path_list = form_path.split(".")
-    module = ".".join(form_path_list[:-1])
+    new_module = ".".join(form_path_list[:-1])
     attr = form_path_list[-1]
     try:
-        mod = import_module(module)
+        mod = import_module(new_module)
     except (ImportError, ValueError):
         raise ImproperlyConfigured('Module configured in LOCKDOWN_FORM (%s) to'
                                    ' contain the form class couldn\'t be '
-                                   'found.' % module)
+                                   'found.' % new_module)
     try:
         form = getattr(mod, attr)
     except AttributeError:
         raise ImproperlyConfigured('The module configured in LOCKDOWN_FORM '
                                    ' (%s) doesn\'t define a "%s" form.'
-                                   % (module, attr))
+                                   % (new_module, attr))
     return form
 
 
