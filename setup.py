4635ef136588c28a9c30e824ea22b3afa0039895
Bruno Alla <alla.brunoo@gmail.com>
2024-02-21 10:13:13 +0100

    Re-add missing comma

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
-        'Framework :: Django'
+        'Framework :: Django',
         'Framework :: Django :: 3.2',
         'Framework :: Django :: 4.0',
         'Framework :: Django :: 4.1',
