0ae7f282fa7616fa5004c0117eaa74713de5df0e
Bruno Alla <alla.brunoo@gmail.com>
2024-02-21 10:34:20 +0100

    Update pre-commit versions

--- lockdown/middleware.py
+++ lockdown/middleware.py
@@ -28,9 +28,9 @@ def get_lockdown_form(form_path):
     try:
         mod = import_module(new_module)
     except (ImportError, ValueError):
-        raise ImproperlyConfigured('Module configured in LOCKDOWN_FORM (%s) to'
-                                   ' contain the form class couldn\'t be '
-                                   'found.' % new_module)
+        raise ImproperlyConfigured("Module configured in LOCKDOWN_FORM (%s) to"
+                                   " contain the form class couldn't be "
+                                   "found." % new_module)
     try:
         form = getattr(mod, attr)
     except AttributeError:
